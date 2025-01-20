a64d293433f24ff26b0daa5e2a402d3aebc1df8e
Column01 <Column01Dev@gmail.com>
2025-01-20 10:49:55 -0500

    Added simple weather tool

---
+++ functions/get_weather.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+
+def get_weather(latitude: str, longitude: str):
+    response = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code")
+    data = response.json()
+    return json.dumps(data['current'])
+
+
+function = get_weather
+function_spec = {
+    "type": "function",
+    "function": {
+        "name": "get_weather",
+        "description": "Get's the weather for the specified location based on latitude and longitude",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "latitude": {"type": "string", "description": "The latitude of the location"},
+                "longitude": {"type": "string", "description": "The longitude of the location"}
+            },
+            "required": ["latitude", "longitude"],
+        },
+    },
+}
